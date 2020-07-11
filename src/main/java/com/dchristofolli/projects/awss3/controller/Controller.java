00cb6b36ea1ef6bfb75e679d56b87fd762db1668
dchristofolli <danielchristofolli@gmail.com>
2020-07-11 17:53:22 -0300

    UploadService refatorada

--- src/main/java/com/dchristofolli/projects/awss3/controller/Controller.java
+++ src/main/java/com/dchristofolli/projects/awss3/controller/Controller.java
@@ -1,11 +1,7 @@
 package com.dchristofolli.projects.awss3.controller;
 
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import service.DownloadService;
@@ -19,17 +15,17 @@ public class Controller {
     private final DownloadService downloadService;
 
     @PostMapping(path = "/upload")
-    public String uploadFile(@RequestPart(value = "file") final MultipartFile multipartFile) {
-        return uploadService.uploadFile(multipartFile);
+    public void uploadFile(@RequestPart(value = "file") final MultipartFile multipartFile) {
+        uploadService.uploadFile(multipartFile);
     }
 
     @GetMapping("/list")
-    public ListObjectsV2Result findAll(){
+    public ListObjectsV2Result findAll() {
         return downloadService.listAll();
     }
 
     @GetMapping("/download/{objectKey}")
-    public void getObject(@PathVariable String objectKey){
+    public void getObject(@PathVariable String objectKey) {
         downloadService.getObject(objectKey);
     }
 }
--- src/main/java/service/UploadService.java
+++ src/main/java/service/UploadService.java
@@ -5,7 +5,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,7 +13,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Slf4j
@@ -23,18 +21,14 @@ import java.util.Objects;
 public class UploadService {
     private final AmazonS3 amazonS3;
 
-    @Async
-    public String uploadFile(final MultipartFile multipartFile) {
-        log.info("File upload in progress.");
+    public void uploadFile(final MultipartFile multipartFile) {
         try {
             final File file = convertMultiPartFileToFile(multipartFile);
             uploadFileToS3Bucket(file);
-            log.info("File upload is completed.");
             Files.delete(Path.of(String.valueOf(file)));
         } catch (final AmazonServiceException | IOException ex) {
-            log.error("Error= {} while uploading file.", ex.getMessage());
+            log.error(ex.getMessage());
         }
-        return "[" + multipartFile.getOriginalFilename() + "] uploaded successfully.";
     }
 
     private File convertMultiPartFileToFile(final MultipartFile multipartFile) {
@@ -42,15 +36,15 @@ public class UploadService {
         try (final FileOutputStream outputStream = new FileOutputStream(file)) {
             outputStream.write(multipartFile.getBytes());
         } catch (final IOException ex) {
-            log.error("Error converting the multi-part file to file= " + ex.getMessage());
+            log.error(ex.getMessage());
         }
         return file;
     }
 
     private void uploadFileToS3Bucket(final File file) {
-        final String uniqueFileName = LocalDateTime.now() + "_" + file.getName();
-        log.info("Uploading file with name= " + uniqueFileName);
-        final PutObjectRequest putObjectRequest = new PutObjectRequest("dchristofolli", uniqueFileName, file);
-        amazonS3.putObject(putObjectRequest);
+        amazonS3.putObject(new PutObjectRequest(
+                "dchristofolli",
+                file.getName(),
+                file));
     }
 }

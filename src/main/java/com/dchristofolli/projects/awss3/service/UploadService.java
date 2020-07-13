a583012e6c55b93a5de3a754e9ed7609f4d2ebf9
dchristofolli <danielchristofolli@gmail.com>
2020-07-13 12:54:32 -0300

    configurações iniciais S3Async

--- src/main/java/com/dchristofolli/projects/awss3/service/UploadService.java
+++ src/main/java/com/dchristofolli/projects/awss3/service/UploadService.java
@@ -1,84 +1,37 @@
 package com.dchristofolli.projects.awss3.service;
 
-import com.dchristofolli.projects.awss3.exception.BadRequestException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class UploadService {
-//    private final AmazonS3 amazonS3;
+    private final S3AsyncClient asyncClient;
 
     @Value("${aws.s3.bucket}")
     private final String bucket;
+    private final S3Configuration s3config;
 
-//    public Mono<Void> uploadFile(Mono<MultipartFile> multipartFile) {
-//        return multipartFile
-//                .flatMap(p -> )
-//    }
-
-//    @Async
-//    public Mono<Void> uploadFile(final MultipartFile multipartFile) {
-//        if(multipartFile.isEmpty())
-//            throw new BadRequestException("Insert a file", HttpStatus.BAD_REQUEST);
-//        log.info("File upload in progress.");
-//        try {
-//            final File file = convertMultiPartFileToFile(multipartFile);
-//            uploadFileToS3Bucket(file);
-//            log.info("File upload is completed.");
-//            Files.delete(Path.of(String.valueOf(file)));
-//        } catch (final AmazonServiceException | IOException ex) {
-//            log.error("Error= {} while uploading file.", ex.getMessage());
-//        }
-//    }
-
-//    public void multipleFileUpload(List<File> fileList) {
-//        if (fileList.size() > 1048576)
-//            log.info("Size: " + fileList.size());
-//        fileList.forEach(file -> fileList.add(new File(String.valueOf(file))));
-//        TransferManager transferManager = TransferManagerBuilder.standard()
-//                .build();
-//        MultipleFileUpload multiFileUpload = transferManager
-//                .uploadFileList(bucket,
-//                        "_",
-//                        new File("."), fileList);
-//        multiFileUpload.getProgress();
-//        try {
-//            multiFileUpload.waitForCompletion();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        transferManager.shutdownNow();
-//    }
-
-    private File convertMultiPartFileToFile(final MultipartFile multipartFile) {
-        final File file = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
-        try (final FileOutputStream outputStream = new FileOutputStream(file)) {
-            outputStream.write(multipartFile.getBytes());
-        } catch (final IOException ex) {
-            log.error(ex.getMessage());
-        }
-        return file;
+    public Mono<Void> uploadFile(Mono<MultipartFile> multipartFile) {
+        CompletableFuture future = asyncClient
+                .putObject(PutObjectRequest.builder()
+                                .bucket(bucket)
+                                .contentLength(length)
+                                .key(fileKey.toString())
+                                .contentType(mediaType.toString())
+                                .metadata(metadata)
+                                .build(),
+                        AsyncRequestBody.fromPublisher(body));
     }
-
-//    private void uploadFileToS3Bucket(final File file) {
-//        amazonS3.putObject(new PutObjectRequest(
-//                bucket,
-//                file.getName(),
-//                file));
-//    }
 }
